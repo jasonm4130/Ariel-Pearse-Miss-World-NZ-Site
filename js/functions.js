1f0e667ce276df95fa0ab189ab916e239d5838c7
jasonm4130 <jasonm4130@gmail.com>
2016-03-16 21:34:03 +1000

    Sponsors Section 50%
    Sponsors section mostly finished no responsive

--- js/functions.js
+++ js/functions.js
@@ -1,15 +1,41 @@
-//$(function(){});
+$(function() {
+  sponsorsBubbleClick();
+});
 
-$(window).scroll(function(){
+function sponsorsBubbleClick() {
+  $('.sponsor').on('click',function() {
+    var $this = $(this),
+        sponsorTop = $(this).position().top,
+        vertMath = -1 * (sponsorTop - 310);
+
+        $this.parent().css('top', + vertMath + 'px');
+
+    $(this).addClass('has-bubble-open')
+      .siblings().removeClass('has-bubble-open')
+  });
+
+}
+
+$(window).scroll(function() {
   postsScroll();
+  startSponsors();
 })
 
 function postsScroll() {
   var wScroll = $(window).scrollTop();
 
-  console.log(wScroll);
-
   $('.post-strip').css('background-position','center -'+ wScroll +'px');
 
+}
+
+function startSponsors () {
+
+  var wScroll = $(window).scrollTop();
+  if($('section.sponsors').offset().top - 500 < wScroll) {
+    $('.sponsorlist').addClass('launched');
+    setTimeout(function(){
+        $('.sponsor:nth-child(3)').addClass('has-bubble-open')
+    }, 400);
+  }
 
 }
